cf262a0cab871dc73d6e05b5f77ee49417308407
kitanaisia <kitanaisia@gmail.com>
2017-01-21 13:47:27 +0900

    Add Member catalog page

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,10 @@
 class Member < ApplicationRecord
   belongs_to :card, foreign_key: :card_id, primary_key: :card_id
+
+  scope :get_by_name, -> (name) {
+    where("name like ?", "%#{name}%")
+  }
+  scope :get_by_rarity, -> (rarity) {
+    where("rarity like ?", "%#{rarity}%")
+  }
 end
